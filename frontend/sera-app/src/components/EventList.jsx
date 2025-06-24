b3f7c9cce147ba996ac8fb585802135264fcaff0
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-24 11:49:00 -0700

    Revised app design with routes to new pages

---
+++ frontend/sera-app/src/components/EventList.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function EventList() {
+  return <Link to={`/events`}>Event List page</Link>;
+}
--- frontend/sera-app/src/components/Login.jsx
+++ frontend/sera-app/src/components/Login.jsx
@@ -3,9 +3,17 @@ import React from "react";
 export default function Login() {
   return (
     <div className="login-container">
-      <form>
+      <h1>WELCOME TO SERAFIN!</h1>
+      <p>Please log in or Create a new User</p>
+      <form className="container">
         <input placeholder="Type username" />
         <input placeholder="Type password" />
+        <select>
+          <option>Choose Role</option>
+          <option value="parent">Parent</option>
+          <option value="student">Student</option>
+          <option value="organize">Organizer</option>
+        </select>
         <button name="register">Register</button>
         <button name="login">Login</button>
         <button name="logout">Logout</button>
