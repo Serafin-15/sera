54a8bde0f4ad4351c70b3eb6c2937da359bf44b4
Alex Reyes-Paredes <areyespa@meta.com>
2025-06-26 09:47:14 -0700

    Implemented user authentication

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,33 @@
-const express = require('express')
-const app = express()
-const PORT = 3000
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
 
-app.use(express.json())
+const app = express();
+const PORT = 3000;
+
+const authRoutes = require("./routes/auth");
+
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
+
+app.use(
+  session({
+    secret: "codepath-sera",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false, httpOnly: true, maxAge: 1000 * 60 * 60 } 
+  })
+);
+
+app.use(authRoutes);
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`)
-})
-app.get('/', (req, res) => {
-  res.send('Welcome to Sera!')
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get("/", (req,res) =>{
+    res.send('Welcome to Sera!')
 })
