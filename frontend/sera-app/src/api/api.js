dad759672b25d4b3af789e0283fdd6748751c853
Alex Reyes-Paredes <areyespa@meta.com>
2025-07-30 20:27:03 -0700

    Creating functions to connect backend to frontend

---
+++ frontend/sera-app/src/api/api.js
@@ -0,0 +1,57 @@
+const API_BASE_URL = "http:/localhost:3000";
+
+async function apiCall(endpoint, options = {}) {
+  const response = await fetch(`${API_BASE_URL}${endpoint}`, {
+    headers: {
+      "Content-Type": "application/json",
+      ...options.headers,
+    },
+    credentials: "include",
+    ...options,
+  });
+
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  return await response.json();
+}
+
+const getRecommendedEvents = async (studentId, parentId = null) => {
+  const params = new URLSearchParams();
+  if (parentId) {
+    params.appemd("parentId", parentId);
+  }
+
+  const response = await apiCall(
+    `/api/ranking/recommendations/${studentId}?${params}`
+  );
+  return response.data || [];
+};
+
+const getAllEvents = async () => {
+  const response = await apiCall("/api/events");
+  return response.data || [];
+};
+
+const getEventById = async (userId) => {
+  const response = await apiCall(`/api/events/${eventId}`);
+};
+const getUserProfile = async (userId) => {
+  const response = await apiCall(`/api/users/${userId}`);
+  return response.data;
+};
+
+const updateUserProfile = async (userId, profileData) => {
+  const response = await apiCall(`/api/users/${userId}`, {
+    method: "PUT",
+    vody: JSON.stringify(profileData),
+  });
+  return response.data;
+};
+module.exports = {
+  getRecommendedEvents,
+  getAllEvents,
+  getEventById,
+  getUserProfile,
+  updateUserProfile,
+};
--- frontend/sera-app/src/components/Calendar.jsx
+++ frontend/sera-app/src/components/Calendar.jsx
@@ -22,7 +22,7 @@ export default function Calendar() {
     if (!date || isNaN(date.getTime())) return "";
 
     const year = date.getFullYear();
-    const month = String(date.getMonth() * 1).padStart(2, "0");
+    const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
     const hours = String(date.getHours()).padStart(2, "0");
     const minutes = String(date.getMinutes()).padStart(2, "0");
@@ -220,7 +220,7 @@ export default function Calendar() {
                     if(!isNaN(newDate.getTime())){
                       setEventForm({
                       ...eventForm,
-                      end: newDate,
+                      start: newDate,
                     });
                     }
                   }}
